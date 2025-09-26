de61f997bc93b020277572fced881f029f54d899
ayushman <ayushmansingh2512@gmail.com>
2025-09-26 20:02:30 +0530

    roll no. 7 and 41  added

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App = () => {
         name: 'Bhargwi raj', 
         rollNo: '003', 
         image: img3,
-        portfolioLink: 'https://bhargwiraj.github.io/p1/'
+        portfolioLink: 'https://devansh1bhardwaj.github.io/PortfolioDev/'
       },
   { 
         id: 5, 
