303ed6a7f5f40246f3aa34f5ca6ae05f27d6f857
ayushman <ayushmansingh2512@gmail.com>
2025-09-26 17:26:14 +0530

    roll no. 41 44 51  and 52  added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import img6 from './assets/Studentimages/6.png';
 import img9 from './assets/Studentimages/9.png';
 import img10 from './assets/Studentimages/10.png';
 import img14 from './assets/Studentimages/14.png';
+import img15 from './assets/Studentimages/15.png';
 import img18 from './assets/Studentimages/18.png';
 import img22 from './assets/Studentimages/22.png';
 import img24 from './assets/Studentimages/24.png';
@@ -87,6 +88,13 @@ const App = () => {
         rollNo: '014', 
         image: img14,
         portfolioLink: 'https://deepaksharma-dadhich.github.io/portfolio/ '
+      },
+             { 
+        id: 15, 
+        name: 'Deepak Tyagi', 
+        rollNo: '015', 
+        image: img15,
+        portfolioLink: 'https://deepaktyagi2002.github.io/p1/'
       },
       { 
         id: 18, 
